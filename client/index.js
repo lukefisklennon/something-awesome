0d0c14feedd5babfa5bcefaec67a029a232e1c59
Luke Fisk-Lennon <luke.fisklennon@gmail.com>
2021-04-02 22:13:27 +1100

    Storage encryption, colours

--- client/index.js
+++ client/index.js
@@ -1,32 +1,142 @@
+const crypto = require("crypto");
 const readline = require("readline");
+const util = require("util");
+const fs = require("fs");
+
 const {table, getBorderCharacters} = require("table");
 require("colors");
 
+let users = [];
+let privateKey = null;
+let password = null;
+let currentChat = null;
+let awaitingInput = false;
+
 const shell = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
-const users = [];
+shell.questionSync = util.promisify(shell.question).bind(shell);
 
-const welcomeText = "Type \"help\" for a list of commands.";
+const ivLength = 16;
+const saltLength = 16;
+const cipherKeyLength = 32;
+const cipherAlgorithm = `aes-${cipherKeyLength * 8}-cbc-hmac-sha256`;
+
+const helpJoiner = "\n   ";
+const passwordChar = "●";
+const passwordTimeout = 2000;
+const colors = ["default", "red", "green", "yellow", "blue", "magenta", "cyan"];
+
+const getBrightColor = ([first, ...rest]) => (
+	`bright${first.toUpperCase() + rest.join("")}`
+);
+
+const getColoredText = (text, color) => (
+	color === "default" ? text : text[getBrightColor(color)]
+);
+
+const welcomeText = () => (
+	`Welcome ${getColoredText(users[0].name, users[0].color)}! ` +
+	`Type "help" for a list of commands.`
+);
+
+const storeDir = (
+	process.env.APPDATA || (
+		process.platform == "darwin"
+		? process.env.HOME + "/Library/Preferences"
+		: process.env.HOME + "/.local/share"
+	)
+) + "/mesh";
+const storeFile = `${storeDir}/store.json`;
 
 const commands = {
 	"help": () => {
-		return `Commands: ${Object.keys(commands).join(", ")}.`;
+		return (
+			`Commands:${helpJoiner}${Object.keys(commands).join(helpJoiner)}.`
+		);
 	},
 	"add [user key]": (args) => {
-		users.push({key: args[0], name: "Unknown".grey, color: null});
+		users.push({key: args[0], name: "Unknown".grey, color: "default"});
 		return `Added user with key "${args[0]}".`;
 	},
 	"remove [user #]": (args) => {
-		users.splice(Number(args[0]) - 1, 1);
-		return `Removed user #${args[0]}.`;
+		const index = Number(args[0]) - 1;
+		if (index !== 0) {
+			users.splice(index, 1);
+			return `Removed user #${args[0]}.`;
+		} else {
+			return "You cannot remove yourself. Type \"reset\" to logout.";
+		}
 	},
 	"chat [user #]": (args) => {
 
+	},
+	[`set name [text]${helpJoiner}set color`]: (args) => {
+
+	},
+	"reset": () => {
+		fs.rmSync(storeFile);
+		setup();
+	},
+	"exit": () => {
+		process.exit();
 	}
 };
+
+const saltedPassword = (password, salt) => (
+	crypto.scryptSync(password, salt, cipherKeyLength)
+);
+
+// Adapted from <https://stackoverflow.com/a/60370205/5583289>
+const encrypt = (text, password) => {
+	const iv = crypto.randomBytes(ivLength);
+	const salt = crypto.randomBytes(saltLength);
+	const key = saltedPassword(password, salt);
+	const cipher = crypto.createCipheriv(cipherAlgorithm, key, iv);
+
+	let encrypted = cipher.update(text);
+	encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+	return [iv, salt, encrypted].map((x) => (x).toString("hex")).join(":");
+}
+
+// Adapted from <https://stackoverflow.com/a/60370205/5583289>
+const decrypt = (text, password) => {
+	const [iv, salt, encrypted] = text.split(":").map(
+		(x) => Buffer.from(x, "hex")
+	);
+
+	const key = saltedPassword(password, salt);
+	const decipher = crypto.createDecipheriv(cipherAlgorithm, key, iv);
+
+	let decrypted = decipher.update(encrypted);
+	decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+	return decrypted.toString();
+}
+
+const storeExists = () => fs.existsSync(storeFile);
+
+const readStore = () => {
+	try {
+		(
+			{users, privateKey} = JSON.parse(decrypt(
+				fs.readFileSync(storeFile, "utf8"), password
+			))
+		);
+	} catch (error) {
+		return error.code;
+	}
+}
+
+const writeStore = () => {
+	if (!fs.existsSync(storeDir)) fs.mkdirSync(storeDir);
+	fs.writeFileSync(storeFile, encrypt(
+		JSON.stringify({users, privateKey}), password
+	));
+}
 
 const onInput = (input) => {
 	const args = input.split(" ");
@@ -45,7 +155,7 @@ const listUsers = () => {
 	let output;
 	const config = {
 		border: getBorderCharacters("norc"),
-		drawHorizontalLine: (index, size) => [0, 1, size].includes(index)
+		drawHorizontalLine: (index, size) => [0, 1, 2, size].includes(index)
 	};
 
 	if (users.length === 0) {
@@ -54,11 +164,19 @@ const listUsers = () => {
 	} else {
 		output = table([
 			["#", "Name", "Key"],
-			...users.map((user, index) => [index + 1, user.name, user.key])
+			...users.map((user, index) => (
+				[index + 1, getColoredText(user.name, user.color), user.key]
+			))
 		], config);
 	}
 
 	console.log(output.trim());
+}
+
+const listColors = () => {
+	console.log(`Colours: ${colors.map(
+		(color) => getColoredText(color, color)
+	).join(", ")}`);
 }
 
 const clearScreen = () => {
@@ -73,10 +191,70 @@ const clearScreen = () => {
 // }
 
 const awaitInput = (lastOutput) => {
+	if (!awaitingInput) return;
+	lastOutput = lastOutput || welcomeText();
+
 	clearScreen();
 	listUsers();
 	console.log(lastOutput);
 	shell.question("> ", onInput);
 }
 
-awaitInput(welcomeText);
+const questionPasswordSync = async (prompt) => {
+	const old_writeToOutput = shell._writeToOutput;
+
+	shell._writeToOutput = (string) => {
+		if (string.trim().length === 0) {
+			shell.output.write(string);
+			return;
+		}
+
+		const split = string.split(prompt);
+
+		if (split.length === 2) {
+			split[1] = passwordChar.repeat(split[1].length);
+		} else {
+			split[0] = passwordChar;
+		}
+
+		shell.output.write(split.join(prompt));
+	};
+
+	const input = await shell.questionSync(prompt);
+	shell._writeToOutput = old_writeToOutput;
+	return input;
+}
+
+const start = async () => {
+	password = await questionPasswordSync("Enter password: ");
+	const error = readStore();
+
+	if (error === "ERR_OSSL_EVP_BAD_DECRYPT") {
+		setTimeout(() => {
+			console.log("Sorry, try again.");
+			start();
+		}, passwordTimeout);
+	} else {
+		awaitingInput = true;
+		awaitInput();
+	}
+}
+
+const setup = async () => {
+	awaitingInput = false;
+	clearScreen();
+
+	const name = await shell.questionSync("Set username: ");
+	listColors();
+	const color = await shell.questionSync("Select user colour: ") || "default";
+	password = await questionPasswordSync("Set password: ")
+
+	users[0] = {key: "abc", name, color};
+	privateKey = "def";
+	writeStore();
+
+	awaitingInput = true;
+	awaitInput();
+}
+
+storeExists() ? start() : setup();
